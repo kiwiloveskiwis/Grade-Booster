faf478514045d1e94651451965bbd0611693cac3
xiaoyuqian2 <xiaoyuqian2@gmail.com>
2018-03-26 19:00:04 -0500

    aggregate grades of sections

--- query.py
+++ query.py
@@ -18,3 +18,13 @@ def remove_favorite(email, course_id):
         DELETE FROM favorite
         WHERE EMAIL = '{email}' AND COURSE_ID = '{course_id}'
     """.format(email=email, course_id=course_id)
+
+def aggregate_sections_grade(subject_name, subject_number):
+    return """
+        SELECT SUM(ap) as ap, SUM(a) as a, SUM(am) as am, SUM(bp) as bp, SUM(b) as b, \
+               SUM(bm) as bm, SUM(cp) as cp, SUM(c) as c, SUM(cm) as cm, SUM(dp) as dp, \
+               SUM(d) as d, SUM(dm) as dm, SUM(f) as f, SUM(w) as w, instructor, semester
+        FROM raw
+        WHERE `subject`='{subject_name}' AND number='{subject_number}'
+        GROUP BY instructor, semester, `subject`, number
+    """.format(subject_name = subject_name, subject_number = subject_number)
