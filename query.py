504e3102eda062c03576e42063b80f9e2679ec34
Kiwi <kiwiloveskiwis@gmail.com>
2018-03-26 19:24:00 -0500

    Change file name

--- query.py
+++ query.py
@@ -4,7 +4,8 @@ def insert_favorite(email, course_id):
         SELECT * FROM (SELECT '{email}', '{course_id}') AS TMP
         WHERE NOT EXISTS (
 	    SELECT * FROM favorite WHERE EMAIL = '{email}' AND COURSE_ID = '{course_id}'
-        ) LIMIT 1""".format(email = email, course_id = course_id)
+        ) LIMIT 1
+        """.format(email = email, course_id = course_id)
 
 def update_favorite(email, old_course_id, new_course_id):
     return """
@@ -18,13 +19,3 @@ def remove_favorite(email, course_id):
         DELETE FROM favorite
         WHERE EMAIL = '{email}' AND COURSE_ID = '{course_id}'
     """.format(email=email, course_id=course_id)
-
-def aggregate_sections_grade(subject_name, subject_number):
-    return """
-        SELECT SUM(ap) as ap, SUM(a) as a, SUM(am) as am, SUM(bp) as bp, SUM(b) as b, \
-               SUM(bm) as bm, SUM(cp) as cp, SUM(c) as c, SUM(cm) as cm, SUM(dp) as dp, \
-               SUM(d) as d, SUM(dm) as dm, SUM(f) as f, SUM(w) as w, instructor, semester
-        FROM raw
-        WHERE `subject`='{subject_name}' AND number='{subject_number}'
-        GROUP BY instructor, semester, `subject`, number
-    """.format(subject_name = subject_name, subject_number = subject_number)
